b1a724b494f09152420185613f571579017e2736
Clayton Cook <claytoncook@Claytons-MacBook-Pro.local>
2018-06-23 01:35:54 -0400

    center sketch

---
+++ app.js
@@ -0,0 +1,8 @@
+var canvas = document.querySelector("canvas");
+
+function center() {
+    canvas.style.top = (screen.height/2 - 450) + "px";
+    canvas.style.left = (screen.width/2 - 450) + "px";
+}
+
+center();
